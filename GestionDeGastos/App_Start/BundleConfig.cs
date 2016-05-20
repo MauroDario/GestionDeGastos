efe14549bcdca664199c8a9812b501f75a8f09ae
Mauro Dario Luna Ayala <mdla.2004@gmail.com>
2016-05-20 16:10:00 -0300

    Se arreglo el js y css

--- GestionDeGastos/App_Start/BundleConfig.cs
+++ GestionDeGastos/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace GestionDeGastos
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +25,13 @@ namespace GestionDeGastos
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/materialize.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/material").Include(
+                     "~/Scripts/jquery.js",
+                     "~/Scripts/materialize.js"
+             ));
         }
     }
 }
