2104046379be86445eba8169917bd799b0e53ffa
Brian Leonel Malavenda <bmalavenda@pragmaconsultores.net>
2016-05-20 14:52:59 -0300

    version con los botones de agregar y el menu de varias opciones todavia sin andar

--- GestionDeGastos/App_Start/BundleConfig.cs
+++ GestionDeGastos/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace GestionDeGastos
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/materialize").Include(
+                  "~/Content/materialize.css",
+                  "~/Scripts/materialize.js"));
         }
     }
 }
